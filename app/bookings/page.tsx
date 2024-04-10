1467e41d5e1c55a2293a2b70692150ede4826f1c
Fernando Neto <fernandonetodeveloper@gmail.com>
2024-04-10 16:44:26 -0300

    feat: ensure booking can be canceled

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -4,14 +4,9 @@ import { authOptions } from "../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 import { db } from "../_lib/prisma";
 import BookingItem from "../_components/booking-item";
-import { isFuture, isPast } from "date-fns";
 
 const BookingsPage = async () => {
-  //recuperar a sessão do usuário (ver se ele está logado ou não )
-
   const session = await getServerSession(authOptions);
-
-  //se ele não estiver logado redirecionar para página de login ou inicial
 
   if (!session?.user) {
     return redirect("/");
@@ -51,9 +46,11 @@ const BookingsPage = async () => {
       <div className="px-5 py-6">
         <h1 className="text-xl font-bold">Agendamentos</h1>
 
-        <h2 className="text-gray-400 uppercase font-bold text-sm mt-6 mb-3">
-          Confirmados
-        </h2>
+        {confirmedBookings.length === 0 && finishedBookings.length === 0 && (
+          <h2 className="text-gray-400 uppercase font-bold text-sm mt-6 mb-3">
+            Confirmados
+          </h2>
+        )}
         <div className="flex flex-col gap-3">
           {confirmedBookings.map((booking) => (
             <BookingItem booking={booking} key={booking.id} />
