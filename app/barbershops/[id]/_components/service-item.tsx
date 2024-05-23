9a071e1d3bf7a8e31ccda09f4b62446a55fb5de3
Fernando Neto <fernandonetodeveloper@gmail.com>
2024-05-23 16:08:09 -0300

    fix: ensure first available day to book a service is tomorrow

--- app/barbershops/[id]/_components/service-item.tsx
+++ app/barbershops/[id]/_components/service-item.tsx
@@ -18,7 +18,7 @@ import React, { useEffect, useMemo, useState } from "react";
 import { generateDayTimeList } from "../_helpers/hours";
 import { format } from "date-fns/format";
 import { saveBooking } from "../_actions/save-booking";
-import { setHours, setMinutes } from "date-fns";
+import { addDays, setHours, setMinutes } from "date-fns";
 import { Loader2 } from "lucide-react";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
@@ -165,7 +165,7 @@ const ServiceItem = ({
                       selected={date}
                       onSelect={handleDateClick}
                       locale={ptBR}
-                      fromDate={new Date()}
+                      fromDate={addDays(new Date(), 1)}
                       styles={{
                         head_cell: {
                           width: "100%",
